071c6c5251f3387015999451c1459c5d8ba12b07
kaikobud <kaikobudsarker@gmail.com>
2020-06-16 22:32:53 +0600

    created django project and app

---
+++ restapienv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kaikobud/Documents/rest_api/Profile-Rest-Api/restapienv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
