08614654022faca54f10fbdce0a9a721ffe50c72
kaikobud <kaikobudsarker@gmail.com>
2020-06-18 00:36:55 +0600

    - built simple profile api

---
+++ profile_api/serializer.py
@@ -0,0 +1,24 @@
+# Created by kaikobud at ১৭/৬/২০
+from rest_framework import serializers
+from profile_api import models
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+
+        return user
--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from profile_api import serializer
+from profile_api import models
+from profile_api import permissions
 
-# Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializer.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
